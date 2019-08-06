b0d023419beba62eab004e01d5b6b15119947e66
liu <2406685038@qq.com>
2019-08-06 20:04:54 +0800

    第五版

--- models/goods.js
+++ models/goods.js
@@ -1,12 +1,12 @@
-import { HTTP } from '../utils/http.js'
+﻿import { HTTP } from '../utils/http.js'
 
 class GoodsModel extends HTTP {
-<<<<<<< HEAD
   //商品
   goodsList() {
     return this.request({
       url: 'goods'
-=======
+    })
+  }
   getList() {
     return this.request({
       url: 'goods',
@@ -15,7 +15,6 @@ class GoodsModel extends HTTP {
   commentList(){
     return this.request({
       url:"ratings"
->>>>>>> ab1cdcd264c55e771768ca4a3e538cad4d9160ab
     })
   }
 }
